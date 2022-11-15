414840a09e1996ce87c522c1b109d2fee997d5f2
zhouston86 <116024224+zhouston86@users.noreply.github.com>
2022-11-15 15:11:02 -0600

    cicd

--- kube-start.sh
+++ kube-start.sh
@@ -1,3 +1,9 @@
+az aks get-credentials --resource-group flask-portfolio-project --name k8-cluster-flask 
+
+kubectl delete deployment flask-app
+kubectl delete deployment pg
+kubectl delete deployment pgadmin
+
 kubectl create -f flask-app-deployment.yaml
 kubectl create -f c-persistentvolumeclaim.yaml
 kubectl create -f pg-deployment.yaml
@@ -6,6 +12,11 @@ kubectl proxy
 kubectl expose deployment/flask-app --type="LoadBalancer" --port 5000
 kubectl expose deployment/pg --type="NodePort" --port 5432
 kubectl expose deployment/pgadmin --type="LoadBalancer" --port 5433
+
+# dump db backup
+PGPOD = $( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
+kubectl exec $PGPOD -- pg_dump postgres > pg/my_pumps_dump.sql
+
 
 # get pg pod and recover db backup
 PGPOD = $( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
