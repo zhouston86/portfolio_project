342a38a46baaaa3f2eda1c07e11acfdc509d04d3
zhouston86 <116024224+zhouston86@users.noreply.github.com>
2022-11-15 16:57:57 -0600

    cicde

--- recover-db.sh
+++ recover-db.sh
@@ -1,5 +1,5 @@
 # get pg pod and recover db backup
-sleep(3)
+sleep 3
 PGPOD=$( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
 echo $PGPOD
 kubectl cp pg/my_pumps_dump2.sql $PGPOD:/tmp
