158b0574098f9c4f1a0c2ae9bd3c2fd18baefbd7
zhouston86 <116024224+zhouston86@users.noreply.github.com>
2022-11-15 15:20:12 -0600

    cicd

--- kube-start.sh
+++ kube-start.sh
@@ -14,11 +14,11 @@ kubectl expose deployment/pg --type="NodePort" --port 5432
 kubectl expose deployment/pgadmin --type="LoadBalancer" --port 5433
 
 # dump db backup
-PGPOD = $( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
-kubectl exec $PGPOD -- pg_dump postgres > pg/my_pumps_dump.sql
+PGPOD=$( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
+kubectl exec $PGPOD -- pg_dump postgres > pg/my_pumps_dump2.sql
 
 
 # get pg pod and recover db backup
-PGPOD = $( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
+PGPOD=$( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
 kubectl cp pg/sql_files/my_pumps_dump.sql $PGPOD:/tmp
 kubectl exec $PGPOD -- psql postgres -f tmp/my_pumps_dump.sql
