75be8e57884388db4158383bcc36c37bacc26113
zhouston86 <116024224+zhouston86@users.noreply.github.com>
2022-11-15 16:04:13 -0600

    ccid

--- kube-start.sh
+++ kube-start.sh
@@ -1,7 +1,15 @@
 
 # dump db backup
 PGPOD=$( kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "pg-")
-kubectl exec $PGPOD -- pg_dump postgres > pg/my_pumps_dump2.sql
+
+LEN=$( $PGPOD | wc -c)
+echo $LEN
+
+if [ $LEN -gt 0 ]
+then
+    echo 'delete'
+    kubectl exec $PGPOD -- pg_dump postgres > pg/my_pumps_dump2.sql
+fi
 
 # kubectl delete deployment flask-app
 # kubectl delete deployment pg
